4d415646272d30a3b4c0d86e6f1790dea9a22739
Dimitrios Pappas <1989dimip@gmx.de>
2015-10-11 22:33:30 +0200

    -added character class

---
+++ src/Character.java
@@ -0,0 +1,25 @@
+
+public class Character {
+	int posX;
+	int posY;
+	
+	public void setPosX(int x)
+	{
+		posX = x;
+	}
+	
+	public void setPosY(int y)
+	{
+		posY = y;
+	}
+	
+	public int getPosX()
+	{
+		return posX;
+	}
+	
+	public int getPosY()
+	{
+		return posY;
+	}
+}
