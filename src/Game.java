b34ba883e3fdad8f1496c59b80c3fa9e9e7cbb03
nicost <nicolas.osterloh@rwth-aachen.de>
2015-10-11 21:00:36 +0200

    simple game logic

---
+++ src/Game.java
@@ -0,0 +1,81 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Game extends JPanel{
+	private static Game tehGame;
+	private int fps = 60; 
+	
+	private int posX = 0;
+	private int posY = 0;
+	private int counter = 0;
+	
+	public static void main(String[] args) {
+		tehGame = new Game();
+        
+        JFrame frame = new JFrame("Reisbaellchen Game");
+		frame.add(tehGame);
+		frame.setSize(600, 600);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		tehGame.run();
+	}
+	
+	/**
+	 * only paint, no logic
+	 */
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g); //clears the content of the last frame
+		Graphics2D g2d = (Graphics2D) g;
+		
+		
+		Color colors[] = {Color.RED, Color.BLUE, Color.YELLOW, Color.GRAY, Color.GREEN, Color.ORANGE};
+		
+		for(int i=0; i < colors.length; i++){
+			g2d.setColor(colors[i]);
+			g2d.fillOval(posX+i*30, posY+i*30, 30, 30);
+		}
+	}
+	
+
+	/**
+	 * game logic
+	 */
+	private void tick(){
+		counter++;
+		posX = (int)(Math.sin(counter/100.0)*70 + 100);
+		posY = (int)(Math.cos(counter/100.0)*70 + 100);
+	}
+	
+	public void run() {
+		long beforeTime, timeDiff, sleep;		
+		beforeTime = System.currentTimeMillis();
+		
+		//loop
+		while (true) {
+			tick();
+			repaint();
+			//passed time
+			timeDiff = System.currentTimeMillis() - beforeTime;
+			//calc sleep time
+			sleep = (1000/fps) - timeDiff;
+			if (sleep < 0) {
+				sleep = 0;
+			}
+
+			try {
+				Thread.sleep(sleep);
+			} catch (InterruptedException e) {
+				System.out.println("Interrupted: " + e.getMessage());
+			}
+			beforeTime = System.currentTimeMillis();
+		}
+	}
+}
