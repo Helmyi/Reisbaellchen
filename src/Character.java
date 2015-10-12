a9b20b3dd60e24fb6888d7d113ef760160b3cfa5
Dimitrios Pappas <1989dimip@gmx.de>
2015-10-12 14:19:31 +0200

    -new player base tiles

--- src/Character.java
+++ src/Character.java
@@ -6,7 +6,7 @@ public class Character extends Entity {
 	private int animationStep;
 	private int animationStepCount;
 	private MovingDirection currentDirection;
-	
+
 	enum MovingDirection {
 		DOWN(0), UP(1), RIGHT(2), LEFT(3);
 		private int code;
@@ -20,39 +20,37 @@ public class Character extends Entity {
 		}
 	}
 
-
 	public Character(Image image) {
 		super(image);
 		init();
 	}
-	
+
 	@Override
 	public void tick() {
 		if (this.isMoving()) {
 			animationStep++;
-			//TODO weitere animationen für angriffe etc hinzufügen und "4" durch variable ersetzen
+			// TODO weitere animationen für angriffe etc hinzufügen und "4"
+			// durch variable ersetzen
 			animationStep %= animationStepCount;
 		}
 	}
 
 	@Override
 	public void paint(Graphics g) {
-		g.drawImage(entityImage, x, y, x + tileWidth, y + tileHeight, animationStep * tileWidth, this.currentDirection.toInt() * tileHeight,
-				(animationStep + 1) * tileWidth, (this.currentDirection.toInt() + 1) * tileHeight, null);
+		g.drawImage(entityImage, x, y, x + tileWidth, y + tileHeight, animationStep * tileWidth,
+				this.currentDirection.toInt() * tileHeight, (animationStep + 1) * tileWidth,
+				(this.currentDirection.toInt() + 1) * tileHeight, null);
 	}
-	
-	public void setMovingDirection(MovingDirection direction)
-	{
+
+	public void setMovingDirection(MovingDirection direction) {
 		this.currentDirection = direction;
 	}
-	
-	public boolean isMoving()
-	{
+
+	public boolean isMoving() {
 		return isMoving;
 	}
-	
-	private void init()
-	{
+
+	private void init() {
 		isMoving = false;
 		animationStep = 0;
 		animationStepCount = 4;
--- src/Entity.java
+++ src/Entity.java
@@ -7,32 +7,28 @@ public abstract class Entity {
 	protected Image entityImage;
 	protected int tileWidth;
 	protected int tileHeight;
-	
-	public Entity(Image image)
-	{
+
+	public Entity(Image image) {
 		this.entityImage = image;
 	}
-	
-	public int getX()
-	{
+
+	public int getX() {
 		return this.x;
 	}
-	
-	public int getY()
-	{
+
+	public int getY() {
 		return this.y;
 	}
-	
-	public void setX(int x)
-	{
+
+	public void setX(int x) {
 		this.x = x;
 	}
-	
-	public void setY(int y)
-	{
+
+	public void setY(int y) {
 		this.y = y;
 	}
 
 	public abstract void tick();
+
 	public abstract void paint(Graphics g);
 }
