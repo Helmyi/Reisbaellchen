37216ef197e5eac3944a26c4716ac7b8254234d6
Fe <fe.tisch88@gmail.com>
2015-11-01 01:06:40 +0100

    add tooltip text to exit button

--- src/main/java/game/menu/GameMenuItem.java
+++ src/main/java/game/menu/GameMenuItem.java
@@ -10,5 +10,10 @@ public class GameMenuItem extends JMenuItem {
 	public GameMenuItem(String optionLabel) {
 		super(optionLabel);
 		this.addActionListener(new MenuActionExit());
+//		this.setRolloverEnabled(true);
+//		this.setRolloverIcon(new ImageIcon("")); TODO
+		this.setToolTipText("Click to exit game");
 	}
+	
+	
 }
